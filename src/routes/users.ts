d35de8a19d4bc69fec3275f440404870a4f6ac21
Maike Castilho <maikecastilho18@gmail.com>
2023-09-14 00:07:47 -0300

    commit.up

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,37 +1,138 @@
-import { FastifyInstance, FastifyRequest } from "fastify";
+import { FastifyInstance } from "fastify";
 import { prisma } from '../lib/prisma'
+import { z } from 'zod'
 
 export async function usersRoutes(app: FastifyInstance) {
     app.get('/users', async () => {
-        const user = await prisma.user.findMany();
+        const user = await prisma.user.findMany({
+
+            orderBy: { //ORDENA O RETORNO EM ORDEM ALFABÉTICA asc(crescente) || desc(decrescente)
+                // name: "asc" ,
+                id: "asc"
+            },
+
+            // where: {//CONDICIONA O RETORNO AO ID: 2
+            //     id: 2
+
+            // },
+
+            // select: { //ESPECIFICA QUAIS CAMPOS SERÃO RETORNADOS DA TABELA: BOOL
+            //     id: true,
+            //     name: true
+
+            // },
+
+            // skip: 2, // Pula os primeiros 2 registros
+            // take: 3,  // Retorna apenas 3 registros
+
+            // cursor: {
+            //     id: 2 //começa a consulta a partir do registro com id 2
+            // },
+
+            // distinct: ['name'], // Retorna nomes de usuários distintos
+
+            // groupBy: {
+            //     name: true, // Agrupa os resultados pelo campo "name"
+            //   }
+
+            // count: true
+        });
         return user;
     })
 
-    app.post('/cadastro', async (request: FastifyRequest) => {
+
+// ###### FILTRANDO USER BY NAME ######
+    app.get('/users/:name', async (request) => {
         try {
-            const { name, email, password} = request.body;
-
-            if (!name || !email || !password) {
-                return {
-                    error: 'Algo de errado'
-                };
-            }
-            const newUser = await prisma.user.create({
-                data: {
-                    name,
-                    email,
-                    password,
-                }
+            const paramsSchema = z.object({
+                name: z.string(),
             });
-            return newUser;
 
-        } catch (error) {
-            return {
-                error: 'Deu ruim ao criar'
+            const { name } = paramsSchema.parse(request.params);
+
+            const user = await prisma.user.findFirst({
+                where: {
+                    name,
+                },
+            });
+
+            if (!user) {
+                return { error: "usuário não encontrado" };
             }
-        }
+
+            return user;
+        } catch (error) {
+            console.error('Erro interno no servidor', error);
+            throw new Error('Error interno do servidor');
+        };
+    });
+
+
+
+    app.post('/users/:id', async (request) => {
+        try {
+            const bodySchema = z.object({
+                id: z.string().refine((val) => !isNaN(Number(val)), {
+                    message: 'O parâmetro deve ser um número'
+                }),
+            });
+
+            const { id } = bodySchema.parse(request.body);
+
+            const user = await prisma.user.findFirst({
+                where: {
+                    id: Number(id),
+                },
+            });
+
+            if (!user) {
+                return { error: "usuário não encontrado" };
+            }
+
+            return user;
+        } catch (error) {
+            console.error('Erro interno no servidor', error);
+            throw new Error('Error interno do servidor');
+        };
+    });
+
+    const userSchema = z.object({
+        name: z.string(),
+        email: z.string(),
+        password: z.string(),
     })
 
+    app.post('/users/cadastro', async (request) => {
+        try{
+            const {name, email, password} = userSchema.parse(request.body);
+
+            const newUser = await prisma.user.create({
+                data: {   
+                    name,
+                    email,
+                    password
+                }
+            });
+            return `Usuário criado com sucesso, id:${newUser.id}`;
+
+        } catch (error) {
+            console.error('Error ao criar user', error);
+            throw new Error('Erro  ao criar o usuario');
+        }
+    });
 
 
-}
+    
+
+
+
+
+
+
+};
+
+
+
+
+
+
