7a624f1f3f2cb8ebacb791321aff6b48bd0a7e88
Maike Castilho <maikecastilho18@gmail.com>
2023-09-24 22:14:17 -0300

    commit.up

--- prisma/migrations/20230923171146_fisrt_migration/migration.sql
+++ prisma/migrations/20230923171146_fisrt_migration/migration.sql
@@ -5,7 +5,10 @@ CREATE TABLE `User` (
     `name_user` VARCHAR(191) NOT NULL,
     `email_user` VARCHAR(191) NOT NULL,
     `password_user` VARCHAR(191) NOT NULL,
+    `register_date` DATETIME(3) NOT NULL,
+    `last_acess` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_token_user_key`(`token_user`),
+    UNIQUE INDEX `User_email_user_key`(`email_user`),
     PRIMARY KEY (`id_user`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
